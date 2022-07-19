73664384a7e55ff9751e3a88cc00135f2d0bd670
Simone Fortunato <frtsmn94@gmail.com>
2022-07-19 11:28:46 +0200

    aggiunta foto in card

--- js/main.js
+++ js/main.js
@@ -9,25 +9,25 @@ let team = [
                 {
                     'nome': 'Angela Caroll',
                     'ruolo': 'Chief Editor',
-                    'foto': 'caroll-chief-editor.jpg'
+                    'foto': 'angela-caroll-chief-editor.jpg'
                 },
 
                 {
                     'nome': 'Walter Gordon',
                     'ruolo': 'Office Manager',
-                    'foto': 'gordon-office-manager.jpg'
+                    'foto': 'walter-gordon-office-manager.jpg'
                 },
 
                 {
                     'nome': 'Angela Lopez',
                     'ruolo': 'Social Media Manager',
-                    'foto': 'lopez-social-media-manager.jpg'
+                    'foto': 'angela-lopez-social-media-manager.jpg'
                 },
 
                 {
                     'nome': 'Scott Estrada',
                     'ruolo': 'Developer',
-                    'foto': 'estrada-developer.jpg'
+                    'foto': 'scott-estrada-developer.jpg'
                 },
 
                 {
@@ -37,21 +37,16 @@ let team = [
                 },
 
             ];
-
-/*for( let chiave in team){
-
-    let persona = document.createElement ('div');
-    console.log(chiave);
-    console.log(team[chiave]);
-}*/
+let immagini = [];
 
 for( i=0; i<team.length; i++){
-    console.log (team[i].nome);
-    container.innerHTML += `
+    container.innerHTML += 
+    `
         <div class='cards'>
             <div class='name'>${team[i].nome}</div>
             <div class='role'>${team[i].ruolo}</div>
-            <div class='pic'>${team[i].foto}</div>
-        </div>
+            <div class='pic'>
+                <img src= "img/${team[i].foto}"/></div>
+            </div>
     `
 }
